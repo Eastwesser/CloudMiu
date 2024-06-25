2c9a726680f7149e0987e11f748f181b57c52903
Dennis Mattews <alttert@mail.ru>
2024-06-25 23:49:13 +0300

    minor photobot update

--- routers/miumiu_custom/miumiu_2_photobot/photobot.py
+++ routers/miumiu_custom/miumiu_2_photobot/photobot.py
@@ -1,5 +1,7 @@
+import asyncio
 import base64
 import json
+import logging
 import os
 import random
 import tempfile
@@ -42,6 +44,9 @@ bot = Bot(token=bot_token)
 dp = Dispatcher()
 
 router = Router(name=__name__)
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 
 class InputFileBytes(InputFile):
