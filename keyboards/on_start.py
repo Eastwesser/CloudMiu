90d038f46729db110ed45ea47c116eb7dc6f0272
Dennis Mattews <alttert@mail.ru>
2024-12-24 01:03:52 +0300

    adjust 11

--- keyboards/on_start.py
+++ keyboards/on_start.py
@@ -218,5 +218,5 @@ def build_yes_or_no_keyboard() -> ReplyKeyboardMarkup:
     builder = ReplyKeyboardBuilder()
     builder.button(text="Yes")
     builder.button(text="No")
-    builder.adjust(10)
+    builder.adjust(11)
     return builder.as_markup(resize_keyboard=True)
