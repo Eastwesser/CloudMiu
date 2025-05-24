2aeec6cade364df15b6706de128ff513038f6abb
Денис Матвеев <alttert@mail.ru>
2025-05-24 14:22:19 +0300

    code upd

--- routers/admin_handlers.py
+++ routers/admin_handlers.py
@@ -8,7 +8,9 @@ from config import settings
 router = Router(name=__name__)
 
 
-@router.message(F.from_user.id.in_(settings.admin_ids), F.text == "secret")
+@router.message(
+    F.from_user.id.in_(settings.admin_ids), F.text == "secret"
+)
 async def secret_admin_message(message: types.Message):
     await message.reply("Hi, admin!")
 
